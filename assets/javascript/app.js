571b2ae059be4f89fb5b31924837d8edcf91a2b2
AChapman <achapman77@gmail.com>
2018-11-24 23:10:59 -0700

    Added Drag and Drop as well as submit and show gifs on pressing Enter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,10 @@
 $(function(){
-
+    //Drag and Drop
+    $( "#buttons-view, #giphs-view" ).sortable({
+        connectWith: ".connectedSortable"
+    }).disableSelection();
+      
+    
 
     var giphs = ["Pong", "Contra", "Metroid", "Super Mario", "Zelda","HALO", "Call of Duty", "Fortnite", "PUBG"]
 
@@ -64,14 +69,17 @@ $(function(){
         $("#buttons-view").empty();
 
         for (var i = 0; i < giphs.length; i++){
-            var b = $("<button>");
+            var b = $("<div>")
+            // b.html($("<button>"));
             b.addClass("giph-btn");
             b.attr("data-name", giphs[i]);
+            b.attr("id", "btn-" + giphs.indexOf(giphs[i]));
             b.text(giphs[i]);
             $("#buttons-view").prepend(b)
         }
     }
 
+    //Toggle still to animate and back using data states
     $("#giphs-view").on("click", ".giph-img", function(){       
         var state = $(this).attr("data-state");
 
@@ -84,6 +92,7 @@ $(function(){
         }
     })
 
+    //Add search term to button array 
     $("#add-giph").on("click", function(event){
         event.preventDefault();
         var giph = $("#giph-input").val().trim()
@@ -93,8 +102,20 @@ $(function(){
         displayGiphInfo();
     });
 
-    $("#buttons-view").on("click", ".giph-btn", displayGiphInfo)
-        
+    $("#buttons-view").on("click", ".giph-btn", displayGiphInfo);
+
+    //Add button from search term by hitting Enter
+    //https://stackoverflow.com/questions/699065/submitting-a-form-on-enter-with-jquery
+    $("#giph-input").keypress(function (e){
+        if (e.which == 13) {
+            jQuery(this).blur();
+            jQuery("#add-giph").focus().click();
+            // clicks on button for search term just entered
+            jQuery("#btn-" + (giphs.length - 1)).focus().click();
+        }
+    });
+    
+    
     
     
 
